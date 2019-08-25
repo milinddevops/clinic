1e710a13ad9fdc4f5b04d86b0c4ce916c3db09cb
Milind <milindchavan.24@gmail.com>
2019-08-24 23:38:39 -0600

    environment vars added

--- Framework/Classes/base/CBaseDatabase.class.php
+++ Framework/Classes/base/CBaseDatabase.class.php
@@ -10,7 +10,7 @@ class CBaseDatabase {
 	public static function loadPortalDb() {
 		require_once( '../../defines.php' );
 
-		$m_lnk_Portal = mysql_connect( DB_HOST, DB_USER, DB_PASSWORD );
+		$m_lnk_Portal = mysql_connect( getenv('DB_HOST'), getenv('DB_USER'), getenv('DB_PASSWORD') );
 		
 		mysql_select_db( DB_NAME, $m_lnk_Portal );
 		
