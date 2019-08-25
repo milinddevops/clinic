efa123071073b15a1becdac470cef9003163d093
Milind <milindchavan.24@gmail.com>
2019-08-25 02:55:05 -0600

    environment vars added.

--- Framework/defines.php
+++ Framework/defines.php
@@ -1,7 +1,7 @@
 <?php
-	define( 'DB_USER', 'root' );
-	define( 'DB_PASSWORD', 'root' );
-	define( 'DB_HOST', 'localhost' );
+	define( 'DB_USER', getenv('DB_USER') );
+	define( 'DB_PASSWORD', getenv('DB_PASSWORD') );
+	define( 'DB_HOST', getenv('DB_HOST') );
 	define( 'DB_NAME', 'ghatge' );
 
 ?>
